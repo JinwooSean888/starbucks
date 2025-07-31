8faa7585a653ca9ac64da8ac16b87d0664678edf
sean80 <kumtle80@gmail.com>
2025-07-31 12:50:15 +0900

    footer:관련수정

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,7 +1,37 @@
 function Footer() {
   return (
     <>
-      <footer></footer>
+      <footer>
+        <div>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/">comp</a>
+            </li>
+            <li>
+              <a href="/">comp</a>
+            </li>
+            <li>
+              <a href="/">comp</a>
+            </li>
+            <li>
+              <a href="/">comp</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li className="footer-li-title">
+              <a href="/">conperate sales</a>
+            </li>
+            <li>
+              <a href="/">단체 및 기업 구매</a>
+            </li>
+            <li>
+              <a href="/">단체 주문 배달 안내</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
